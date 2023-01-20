7190862ca65b5b75b6cd556e2c82e32c7318f789
PabloChacon1997 <pabloandres120297@gmail.com>
2023-01-20 13:19:26 -0500

    Conversion de imagenes

--- 11.-cafeteria/gulpfile.js
+++ 11.-cafeteria/gulpfile.js
@@ -1,7 +1,14 @@
 const { src, dest, watch, series } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+
+// IMG
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
   // compilar sass
@@ -14,12 +21,38 @@ function css(done) {
   done();
 }
 
-function dev() {
-  watch("src/scss/**/*.scss", css);
+function imagenes() {
+  return src("src/img/**/*")
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest("build/img"));
 }
 
-// exports.dev = dev;
-exports.default = series(css, dev);
+function versionwebp() {
+  const opciones = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{png,jpg}")
+    .pipe(webp(opciones))
+    .pipe(dest("build/img"));
+}
+function versionavif() {
+  const opciones = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{png,jpg}")
+    .pipe(avif(opciones))
+    .pipe(dest("build/img"));
+}
+
+function dev() {
+  watch("src/scss/**/*.scss", css);
+  watch("src/img/**/*", imagenes);
+}
+
+exports.imagenes = imagenes;
+exports.versionwebp = versionwebp;
+exports.versionavif = versionavif;
+exports.default = series(imagenes, versionwebp, versionavif, css, dev);
 
 // series - se inicia una tarea y hasta que finaliza inicia la siguiente
 // paralel - Todas inician al mismo tiempo
