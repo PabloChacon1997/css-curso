8a147c3a0af31a80e053f046c0142f1f2a4e7ea7
PabloChacon1997 <pabloandres120297@gmail.com>
2023-02-14 17:01:39 -0500

    Fin del proyecto 15

---
+++ 15-realState/js/app.js
@@ -0,0 +1,8 @@
+const imagenes = document.querySelectorAll(".propiedad__imagen");
+
+window.addEventListener("scroll", () => {
+  imagenes.forEach((imagen) => {
+    const scroll = this.scrollY / -20;
+    imagen.style.backgroundPositionY = `${scroll}px`;
+  });
+});
